97935c1d3d47a27a9d0f3fc2badd84c1711d9522
Andrew Palmer <andrew.pa@outlook.com>
2024-08-22 21:10:56 -0700

    cache items in database

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import feedparser
 import time
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///rss_feeds.db"
@@ -11,7 +12,25 @@ db = SQLAlchemy(app)
 
 class Feed(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(500), nullable=False)
+    url = db.Column(db.String(512), nullable=False)
+    title = db.Column(db.String(256), nullable=True)
+    etag = db.Column(db.String(128), nullable=True)
+    modified = db.Column(db.String(128), nullable=True)
+    items = db.relationship(
+        "Item", backref="feed", lazy=True, cascade="all, delete-orphan"
+    )
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), nullable=False)
+    link = db.Column(db.String(512), nullable=False)
+    published = db.Column(db.DateTime, nullable=False, index=True)
+    description = db.Column(db.Text, nullable=True)
+    author = db.Column(db.String(128), nullable=True)
+    feed_id = db.Column(
+        db.Integer, db.ForeignKey("feed.id"), nullable=False, index=True
+    )
 
 
 with app.app_context():
@@ -22,47 +41,81 @@ with app.app_context():
 def format_date(value, format="%d %B %Y, %I:%M %p"):
     if value is None:
         return ""
-    return datetime(*value[:6]).strftime(format)
+    return value.strftime(format)
+
+
+def datetime_from_time(t):
+    return datetime.fromtimestamp(time.mktime(t))
+
+
+def update_feed(feed, data):
+    feed.title = data.feed.title
+    feed.etag = data.get("etag", None)
+    feed.modified = data.get("modified", None)
+    feed_pub_date = data.feed.get(
+        "published_parsed", data.feed.get("updated_parsed", time.localtime())
+    )
+    most_recent_item = db.session.query(Item.published).filter_by(
+        feed_id=feed.id
+    ).order_by(Item.published.desc()).first() or (
+        datetime.now() - relativedelta(months=2)
+    )
+    db.session.add_all(
+        filter(
+            lambda item: item.published >= most_recent_item,
+            (
+                Item(
+                    title=entry.title or "?",
+                    link=entry.link,
+                    published=datetime_from_time(
+                        entry.get(
+                            "published_parsed",
+                            entry.get("updated_parsed", feed_pub_date),
+                        )
+                    ),
+                    description=entry.get("description", None),
+                    author=entry.get("author", None),
+                    feed=feed,
+                )
+                for entry in data.entries
+            ),
+        )
+    )
+
+
+def update_feeds():
+    num_failed = 0
+    num_updated = 0
+    num_feeds = 0
+    for feed in Feed.query:
+        num_feeds += 1
+        try:
+            data = feedparser.parse(feed.url, etag=feed.etag, modified=feed.modified)
+            if len(data.entries) > 0:
+                update_feed(feed, data)
+                num_updated += 1
+        except Exception as e:
+            print(f"failed to load feed {feed}: {e}")
+            num_failed += 1
+    db.session.commit()
+    return num_feeds, num_failed, num_updated
 
 
 @app.route("/")
 def index():
     start_time = time.time()
-    feeds = Feed.query.all()
-    num_failed = 0
-    all_items = []
-    for feed in feeds:
-        try:
-            parsed_feed = feedparser.parse(feed.url)
-            feed_pub_date = parsed_feed.feed.get(
-                "published_parsed", parsed_feed.get("updated_parsed", time.localtime())
-            )
-            for entry in parsed_feed.entries:
-                all_items.append(
-                    {
-                        "feed_name": parsed_feed.feed.title,
-                        "title": entry.title,
-                        "link": entry.link,
-                        "published": entry.get(
-                            "published_parsed",
-                            entry.get("updated_parsed", feed_pub_date),
-                        ),
-                        "description": entry.description,
-                        "author": entry.get("author", None),
-                    }
-                )
-        except Exception as e:
-            print(f"failed to load feed {feed}: {e}")
-            num_failed += 1
-    all_items.sort(key=lambda x: x["published"], reverse=True)
-    end_time = time.time()
+    (num_feeds, num_failed, num_updated) = update_feeds()
+    update_time = time.time()
+    items = db.session.query(Item).order_by(Item.published.desc()).limit(128).all()
+    load_time = time.time()
     return render_template(
         "index.html",
-        items=all_items[:100],
-        fetch_time=round(end_time - start_time, 3),
-        feed_count=len(feeds),
-        item_count=len(all_items),
+        items=items,
+        load_time=round(load_time - update_time, 3),
+        update_time=round(update_time - start_time, 3),
+        feed_count=num_feeds,
         failed_feed_count=num_failed,
+        updated_feed_count=num_updated,
     )
 
 
